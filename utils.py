03139ca2ff01500ea6238e99af90a8d96d8b41f2
JKeun <jaekeunprk@gmail.com>
2022-06-28 16:55:04 +0900

    Add 07-linear-model

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import math
 import random
+import numpy as np
 
 
 # math
@@ -24,4 +25,25 @@ def generate_gaussian_vector(size, mu=0, sigma=1):
     res = [0] * size
     for k in range(size):
         res[k] = random.gauss(mu, sigma)
-    return res 
+    return res
+
+
+# solve linear models
+def solve(X, y, alpha=0):
+    X_t = X.T
+    X_t_X = np.dot(X_t, X)
+    X_t_y = np.dot(X_t, y)
+    X_t_X_inv = np.linalg.inv(X_t_X + alpha * np.eye(len(X_t)))
+    w_hat = np.dot(X_t_X_inv, X_t_y)
+    return w_hat
+
+
+# evaluation metrics
+def mse(gt, pred):
+    return np.sum((gt - pred) ** 2) / len(gt)
+
+def mae(gt, pred):
+    return np.sum(np.abs(gt - pred)) / len(gt)
+
+def r2(gt, pred):
+    return 1 - mse(gt, pred) / np.var(gt)
