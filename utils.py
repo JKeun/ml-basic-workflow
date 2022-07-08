59943ea1ab6b9caa61aec9c4d36c12e45cefcef3
JKeun <jaekeunprk@gmail.com>
2022-07-08 22:16:41 +0900

    Add 10-decision-tree notebook

--- utils.py
+++ utils.py
@@ -1,6 +1,14 @@
 import math
 import random
 import numpy as np
+import pandas as pd
+import pydotplus
+import random
+
+from six import StringIO
+from sklearn import tree
+from sklearn.tree import DecisionTreeRegressor
+from IPython.display import Image
 
 
 # math
@@ -27,8 +35,48 @@ def generate_gaussian_vector(size, mu=0, sigma=1):
         res[k] = random.gauss(mu, sigma)
     return res
 
+def make_linear_dataset(num_samples=1000,
+                        num_features=20,
+                        target_noise_sigma=5,
+                        add_plus_noise=False,
+                        seed=42
+                        ):
+    np.random.seed(seed)
+    
+    X = np.random.uniform(10, 50, size=(num_samples, num_features))
+    w = np.random.randn(num_features)
+    y = np.dot(X, w) + target_noise_sigma * np.random.randn(num_samples)
+    
+    if add_plus_noise:
+        for _ in range(5):
+            y[np.random.randint(num_samples)] += 10000 * np.random.rand()
+    
+    return X, y
 
-# solve linear models
+def make_non_linear_dataset(num_samples=1000,
+                            num_features=20,
+                            target_noise_sigma=5,
+                            seed=42
+                            ):
+    np.random.seed(42)
+    
+    X = np.random.uniform(10, 50, size=(num_samples, num_features))
+    w = np.random.randn(num_features)
+    
+    # square some random input features
+    sqr_idxs = np.random.choice(range(num_features), int(num_features / 2))
+    X[:, sqr_idxs] = X[:, sqr_idxs] ** 2
+    
+    # sine some random input features
+    sin_idxs = np.random.choice(range(num_features), int(num_features / 2))
+    X[:, sin_idxs] = np.sin(X[:, sin_idxs])
+    
+    y = np.sqrt(np.abs(np.dot(X, w))) + target_noise_sigma * np.random.randn(num_samples)
+    
+    return X, y
+
+
+# linear models
 def solve(X, y, alpha=0):
     X_t = X.T
     X_t_X = np.dot(X_t, X)
@@ -49,14 +97,44 @@ def solve_predict_norm(X, y, alpha=0):
     norm_w_hat = solve(norm_Xs, norm_ys, alpha)
     norm_y_hat = np.dot(norm_Xs, norm_w_hat)
     
-    # denormalize
+    # denormalize, return to real scale
     y_hat = mean_y + sd_y * norm_y_hat
     return y_hat, norm_w_hat
+
+def ridge(X, y, alpha=0.01):
+    return solve(X, y, alpha=alpha)
+
+def norm_ridge(X, y, alpha=0.01):
+    return solve_predict_norm(X, y, alpha=alpha)
+
+
+# tree model
+def make_trained_decision_tree(X, y, depth=4):
+    
+    tree = DecisionTreeRegressor(max_depth=depth)
+    tree.fit(X, y)
+    return tree
+
+def decision_tree_predict(tree, X):
+    return tree.predict(X)
+
+def get_tree_image(dt):
+    dot_data = StringIO()
+    
+    tree.export_graphviz(dt, out_file=dot_data,
+                         filled=True, rounded=True,
+                         special_characters=True)
+    
+    graph = pydotplus.graph_from_dot_data(dot_data.getvalue())
+    return Image(graph.create_png())
 
 
 # evaluation metrics
 def mse(gt, pred):
     return np.mean((gt - pred) ** 2)
+
+def rmse(gt, pred):
+    return np.sqrt(mse(gt, pred))
 
 def mae(gt, pred):
     return np.mean(abs(gt - pred))
@@ -85,3 +163,10 @@ def ks(gt, pred):
     cdf0 = np.cumsum(1-corr_gt[sort_idxs]) / sum(1-corr_gt)
     ks = np.max(np.abs(cdf0 - cdf1))
     return ks
+
+def compute_evaluation_series(gt, pred, name=None):
+    if name is None:
+        pass
+    data = [mse(gt, pred), mae(gt, pred), r2(gt, pred), gini(gt, pred)]
+    index = ["MSE", "MAE", "R^2", "Gini"]
+    return pd.Series(data, index, name=name)
